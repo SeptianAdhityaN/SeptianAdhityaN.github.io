75bc97408d9a097ecd0bdd022a22274ad09b6ef2
SeptianAdhityaN <septiannugroho73@gmail.com>
2024-07-02 13:47:18 +0700

    Fix Bug Responsive

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,5 +3,5 @@ function Menu(e) {
 
   e.name === "menu-outline"
     ? ((e.name = "close-outline"), list.classList.remove('mt-[-1000px]'), list.classList.remove('opacity-0'), list.classList.add('mt-[100px]'), list.classList.add('opacity-100'))
-    : ((e.name = "menu-outline"), list.classList.remove('mt-[100px]'), list.classList.remove('opacity-100'), list.classList.add('mt-[-1000px]'), list.classList.add('opacity-0'));
+    : ((e.name = "menu-outline"), list.classList.remove('mt-[100px]'), list.classList.remove('opacity-100'), list.classList.add('mt-[-1000px]'), list.classList.add('opacity-0'))
 }
